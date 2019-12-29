e0b28ab1eda929affc3f4b7a90ad9cc8c4030ec5
silence <yangkemuyi@sina.com>
2019-12-29 17:42:11 +0800

    增加事件统计

---
+++ LTGameSDKUIFacebook/src/main/java/com/gentop/ltsdk/facebook/FacebookEventManager.java
@@ -0,0 +1,51 @@
+package com.gentop.ltsdk.facebook;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import com.facebook.FacebookSdk;
+import com.facebook.LoggingBehavior;
+import com.facebook.appevents.AppEventsConstants;
+import com.facebook.appevents.AppEventsLogger;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
+public class FacebookEventManager {
+    /**
+     * 开始
+     */
+    public static void start() {
+        FacebookSdk.setIsDebugEnabled(true);
+        FacebookSdk.addLoggingBehavior(LoggingBehavior.APP_EVENTS);
+    }
+
+    /**
+     * 注册
+     */
+    public static void register(Context context, int code) {
+        AppEventsLogger logger = AppEventsLogger.newLogger(context);
+        Bundle params = new Bundle();
+        if (code == 0) {
+            params.putString(AppEventsConstants.EVENT_PARAM_REGISTRATION_METHOD, "Facebook");
+        } else if (code == 1) {
+            params.putString(AppEventsConstants.EVENT_PARAM_REGISTRATION_METHOD, "Google");
+        } else if (code == 2) {
+            params.putString(AppEventsConstants.EVENT_PARAM_REGISTRATION_METHOD, "Guest");
+        }
+        logger.logEvent(AppEventsConstants.EVENT_NAME_COMPLETED_REGISTRATION, params);
+    }
+
+    /**
+     * 内购
+     */
+    public static void recharge(Context context, long money, String unit, String orderID) {
+        AppEventsLogger logger = AppEventsLogger.newLogger(context);
+        BigDecimal decimal = BigDecimal.valueOf(money);
+        Currency currency = Currency.getInstance(unit);
+        Bundle parameters = new Bundle();
+        parameters.putString("LTOrderId", orderID);
+        logger.logPurchase(decimal, currency, parameters);
+    }
+
+}
